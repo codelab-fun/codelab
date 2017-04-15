a283e15bde862dc7f2794b2a29c141ab3ef48404
Victor Mikhaylov <vic.m@mail.ru>
2017-04-14 21:01:13 -0400

    footer with styles

--- src/app/shared.module.ts
+++ src/app/shared.module.ts
@@ -5,20 +5,24 @@ import {PresentationComponent} from './presentation/presentation/presentation.co
 import {ResizeDirective} from './presentation/resize/resize.directive';
 import {ShortcutsDirective} from './presentation/shortcuts/shortcuts.directive';
 import {ProgressBarComponent} from './presentation/progress-bar/progress-bar.component';
+import { FooterComponent } from './presentation/footer/footer.component';
 
 
 @NgModule({
   declarations: [
     PresentationComponent,
     SlideComponent,
+    FooterComponent,
     ResizeDirective,
     ShortcutsDirective,
-    ProgressBarComponent
+    ProgressBarComponent,
+    FooterComponent
   ],
 
   exports: [
     PresentationComponent,
     SlideComponent,
+    FooterComponent,
     ProgressBarComponent
   ],
   imports: [BrowserModule],
