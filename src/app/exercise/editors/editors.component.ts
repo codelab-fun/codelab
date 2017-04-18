b799e1440a6e86026bd78362c407761f7734079a
UmaMoiseenko <uma@KRG-F29F-Uma.local>
2017-04-17 20:00:25 -0400

    added multiple tabs to view files

--- src/app/exercise/editors/editors.component.ts
+++ src/app/exercise/editors/editors.component.ts
@@ -5,7 +5,7 @@ import {FileConfig} from '../interfaces/file-config';
 @Component({
   selector: 'app-editors',
   templateUrl: './editors.component.html',
-  styleUrls: ['./editors.component.css']
+  styleUrls: ['./editors.component.scss']
 })
 export class EditorsComponent {
   @Input() public files: Array<any>;
@@ -13,24 +13,43 @@ export class EditorsComponent {
   @Output() public onToggle: EventEmitter<any> = new EventEmitter<FileConfig>();
   @Output() public onLoadSolution: EventEmitter<any> = new EventEmitter<FileConfig>();
   private debug: boolean;
-
-
-  trackFile(index, file) {
-    return file.path;
-  }
-
-  get visibleFiles() {
-    return this.files.filter(file => !file.hidden);
-  }
-
-  onCodeChange(change) {
-    this.onChanges.emit(change);
-  }
+  private currentFile;
 
   constructor() {
     // TODO
     this.debug = true;
   }
 
+  onCodeChange(change) {
+    this.onChanges.emit(change);
+  }
 
+  ngOnInit() {
+    this.currentFile = this.visibleFiles[0]
+  }
+
+  get visibleFiles() {
+    return this.files.filter(file => !file.hidden);
+  }
+
+  getFileName(file) {
+    return file.path.replace(/^.*[\\\/]/, '');
+  }
+
+  getOpenedFile() {
+    return this.currentFile;
+  }
+
+  isOpenFile(file) {
+    if (!file || !this.currentFile) { return; }
+    return file.path == this.currentFile.path;
+  }
+
+  showFile(file) {
+    this.currentFile = file;
+  }
+
+  trackFile(index, file) {
+    return file.path;
+  }
 }
