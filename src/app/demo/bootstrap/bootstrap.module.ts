b6272d86bf9b1727c7a15fe0b09484a0b6c41cd0
Aziz <ayokubjonov@hudsonhs.com>
2017-04-25 21:43:46 -0400

    add feedback widget

--- src/app/demo/bootstrap/bootstrap.module.ts
+++ src/app/demo/bootstrap/bootstrap.module.ts
@@ -5,8 +5,8 @@ import {SlidesRoutes} from '../../presentation/slide-routes';
 
 import {ExerciseModule} from '../../exercise/exersice.module';
 import {PresentationModule} from '../../presentation/presentation.module';
-import {FeedbackModule} from "../../feedback/feedback.module";
 import {BrowserWindowModule} from '../../browser-window/browser-window.module';
+import {FeedbackModule} from '../../feedback/feedback.module';
 
 
 const routes = RouterModule.forChild(
@@ -14,7 +14,7 @@ const routes = RouterModule.forChild(
 );
 
 @NgModule({
-  imports: [routes, PresentationModule, ExerciseModule, FeedbackModule, BrowserWindowModule, FeedbackModule],
+  imports: [routes, PresentationModule, ExerciseModule, BrowserWindowModule, FeedbackModule],
   declarations: [BootstrapComponent],
   exports: [BootstrapComponent]
 })
