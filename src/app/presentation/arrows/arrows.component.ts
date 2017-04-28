89ba42f7d165818673260b29d52ffbddb7e171d2
Namik Shehu <namikus@hotmail.com>
2017-04-28 00:05:36 -0400

    Updates arrows.component.ts to hide when there is no next or previous slide.

--- src/app/presentation/arrows/arrows.component.ts
+++ src/app/presentation/arrows/arrows.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {PresentationComponent} from '../presentation/presentation.component';
+import { Component, OnInit } from '@angular/core';
+import { PresentationComponent } from '../presentation/presentation.component';
 
 @Component({
   selector: 'app-arrows',
@@ -16,6 +16,13 @@ export class ArrowsComponent implements OnInit {
     this.presentation.nextSlide();
   }
 
+  canGoNext(): boolean {
+    return this.presentation.activeSlideIndex + 1 < this.presentation.totalSlides;
+  }
+
+  canGoPrevious(): boolean {
+    return this.presentation.activeSlideIndex !== 0;
+  }
   constructor(private presentation: PresentationComponent) {
 
   }
