f2e9ddb01d366635cd6af1f66d29bda3cffea3c8
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-04-28 13:56:24 -0400

    Add feedback to the TypeScript module

--- src/app/demo/typescript/typescript.module.ts
+++ src/app/demo/typescript/typescript.module.ts
@@ -4,7 +4,8 @@ import {RouterModule} from '@angular/router';
 import {SlidesRoutes} from '../../presentation/slide-routes';
 import {ExerciseModule} from '../../exercise/exersice.module';
 import {PresentationModule} from 'app/presentation/presentation.module';
-import {FeedbackModule} from "../../feedback/feedback.module";
+import {FeedbackModule} from '../../feedback/feedback.module';
+
 
 const routes = RouterModule.forChild(
   SlidesRoutes.get(TypescriptComponent)
