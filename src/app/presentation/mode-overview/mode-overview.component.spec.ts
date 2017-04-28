52fd6b186f87ac2c262761a79aa924818228e236
Dan Fusaro <dan@industrialcolor.com>
2017-04-28 12:34:17 -0400

    Overview/Print mode fixes
    Switched to string-based enum for ease of router integration
    Made naming more succinct
    Moved routing logic out of the component and into an independent
    directive, following how slide routing works
    Use direct enum reference in all templates

--- src/app/presentation/mode-overview/mode-overview.component.spec.ts
+++ src/app/presentation/mode-overview/mode-overview.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ModeOverviewComponent } from './mode-overview.component';
 
-import { OverviewComponent } from './overview.component';
 
 describe('OverviewComponent', () => {
-  let component: OverviewComponent;
-  let fixture: ComponentFixture<OverviewComponent>;
+  let component: ModeOverviewComponent;
+  let fixture: ComponentFixture<ModeOverviewComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ OverviewComponent ]
+      declarations: [ ModeOverviewComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(OverviewComponent);
+    fixture = TestBed.createComponent(ModeOverviewComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
