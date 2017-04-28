2672f0dd74b31f64cea32bd32d24f09d31b7cc0d
panforrest <guoqianp@gmail.com>
2017-04-28 19:32:33 -0400

    add note icon to all pages

--- src/app/demo/dependency-injection/dependency-injection.module.ts
+++ src/app/demo/dependency-injection/dependency-injection.module.ts
@@ -2,7 +2,7 @@ import {NgModule} from '@angular/core';
 import {DependencyInjectionComponent} from './dependency-injection.component';
 import {RouterModule} from '@angular/router';
 import {SlidesRoutes} from '../../presentation/slide-routes';
-
+import {FeedbackModule} from "../../feedback/feedback.module";
 import {ExerciseModule} from '../../exercise/exersice.module';
 import {PresentationModule} from '../../presentation/presentation.module';
 
@@ -12,7 +12,7 @@ const routes = RouterModule.forChild(
 );
 
 @NgModule({
-  imports: [routes, PresentationModule, ExerciseModule],
+  imports: [routes, PresentationModule, ExerciseModule, FeedbackModule],
   declarations: [DependencyInjectionComponent],
   exports: [DependencyInjectionComponent]
 })
--- src/app/demo/templates/templates.module.ts
+++ src/app/demo/templates/templates.module.ts
@@ -5,13 +5,14 @@ import {SlidesRoutes} from '../../presentation/slide-routes';
 import {TemplatesComponent} from './templates.component';
 import {ExerciseModule} from '../../exercise/exersice.module';
 import {PresentationModule} from '../../presentation/presentation.module';
+import {FeedbackModule} from "../../feedback/feedback.module";
 
 const routes = RouterModule.forChild(
   SlidesRoutes.get(TemplatesComponent)
 );
 
 @NgModule({
-  imports: [routes, PresentationModule, ExerciseModule],
+  imports: [routes, PresentationModule, ExerciseModule, FeedbackModule],
   declarations: [TemplatesComponent],
   exports: [TemplatesComponent]
 })
