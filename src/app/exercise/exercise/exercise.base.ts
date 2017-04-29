2f24cf8e3ecf82405c3b1a0e64cd3ecfc9c94305
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-04-29 14:22:16 -0400

    Display "Exercise is solved" message in an exercise is solved

--- src/app/exercise/exercise/exercise.base.ts
+++ src/app/exercise/exercise/exercise.base.ts
@@ -7,6 +7,7 @@ import {Subscription} from 'rxjs/Subscription';
 export class ExerciseBase implements OnDestroy {
   @Input() public config: ExerciseConfig;
   running = false;
+  solved = false;
   private onActiveUsubscribe: Subscription;
 
 
@@ -30,6 +31,9 @@ export class ExerciseBase implements OnDestroy {
     }
 
     if (event.data.type === 'testEnd') {
+      if (this.config.tests.length && this.config.tests.every(test => test.pass)) {
+        this.solved = true;
+      }
       this.running = false;
     }
 
