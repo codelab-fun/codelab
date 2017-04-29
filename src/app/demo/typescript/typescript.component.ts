41e4f77a6d393c27806ad2dc39ee7ccf98287b01
tessaSAC <tessaSAC@users.noreply.github.com>
2017-04-28 20:52:34 -0400

    componentizes title-slide style and adds ripple animation

--- src/app/demo/typescript/typescript.component.ts
+++ src/app/demo/typescript/typescript.component.ts
@@ -7,10 +7,11 @@ import {ng2tsConfig} from '../../../../ng2ts/ng2ts';
   templateUrl: './typescript.component.html',
   styleUrls: ['./typescript.component.css']
 })
-export class TypescriptComponent {
 
+export class TypescriptComponent {
   title = 'TypeScript';
   description = 'Human give me attention meow ask to go outside and ask to come inside and ask to go outside and ask to come inside so playing with balls of wool.';
+  prereqs = 'Components, Dependency Injection';
 
   exercises = [
     ng2tsConfig.milestones[0].exercises[1]
--- src/app/presentation/title-slide/title-slide.component.ts
+++ src/app/presentation/title-slide/title-slide.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-title-slide',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./title-slide.component.css']
 })
 export class TitleSlideComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() description: string;
+  @Input() prereqs: string;
 
   constructor() { }
 
