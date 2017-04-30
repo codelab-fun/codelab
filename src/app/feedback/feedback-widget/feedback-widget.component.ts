4254a286d5065c18cfb4ffcd4da000d8d2448692
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-04-30 16:37:52 -0400

    Add feedback count and move things around in the feedback widget

--- src/app/feedback/feedback-widget/feedback-widget.component.ts
+++ src/app/feedback/feedback-widget/feedback-widget.component.ts
@@ -5,6 +5,8 @@ import {Subscription} from 'rxjs/Subscription';
 import {Message} from '../message';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
+import {combineLatest} from 'rxjs/observable/combineLatest';
+import 'rxjs/add/operator/map';
 
 
 @Component({
@@ -13,51 +15,41 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./feedback-widget.component.css']
 })
 export class FeedbackWidgetComponent implements OnInit, OnDestroy {
-  private isOpen: boolean;
-  private initialized;
+  messages: Array<Message> = [];
+
   private repoSubscription: Subscription;
   private routeSubscription: Subscription;
 
 
   statusMessage = '';
   error = false;
+  open: boolean;
 
   formGroup: FormGroup;
   repo$: FirebaseListObservable<any>;
-  items: Message;
-
-  set open(value: boolean) {
-    this.isOpen = value;
-    if (!this.repo$) {
-      this.initData();
-    }
-  }
-
-  get open(): boolean {
-    return this.isOpen;
-  }
 
   constructor(private angularFire: AngularFire,
               private el: ElementRef,
               private fb: FormBuilder,
               private router: Router,
               private activatedRoute: ActivatedRoute) {
+    this.repo$ = this.angularFire.database.list('/feedback');
 
-    this.activatedRoute.url.subscribe(() => {
-      if (this.initialized) {
-        // Get new data for route
-        this.repoSubscription.unsubscribe();
-        this.initData();
-      }
-    });
+    combineLatest(this.activatedRoute.url, this.repo$)
+      .map(([_, messages]) => {
+        return (messages as Array<Message>)
+          .filter(m => m.href.toLowerCase() === this.router.url.toLowerCase()).sort()
+      }).subscribe(messages => this.messages = messages);
   }
 
   ngOnInit() {
     this.formGroup = this.fb.group({
       comment: ['', Validators.required],
-      name: ['', Validators.required],
-      email: ['', []]
+      name: [localStorage.getItem('userName') || '', Validators.required],
+      email: [localStorage.getItem('userEmail') || '', []]
     });
+
+
   }
 
   ngOnDestroy() {
@@ -66,18 +58,6 @@ export class FeedbackWidgetComponent implements OnInit, OnDestroy {
     }
     this.routeSubscription.unsubscribe();
   }
-
-  initData() {
-    this.repo$ = this.angularFire.database.list('/feedback');
-    // Get all feedback for this url, sorted by date, newest first
-    this.repoSubscription = this.repo$.subscribe(values => {
-      this.items = values.map(m => m as Message)
-        .filter(m => m.href.toLowerCase() === this.router.url.toLowerCase())
-        .sort((a, b) => a > b ? -1 : 1);
-    });
-    this.initialized = true;
-  }
-
 
   @HostListener('window:mousedown')
   handleDialogClose() {
@@ -94,19 +74,18 @@ export class FeedbackWidgetComponent implements OnInit, OnDestroy {
     this.open = !this.open;
   }
 
-  getWidth() {
-    return this.open ? 25 : 100;
-  }
-
   submit() {
     const message: Message = this.formGroup.getRawValue();
-    message.comment = this.htmlEscape(message.comment);
     message.href = this.router.url;
     message.timestamp = new Date().toUTCString();
     message.header = this.getHeaderText();
+
+    // TODO: Consider storing in firebase instead?
+    localStorage.setItem('userName', message.name);
+    localStorage.setItem('userEmail', message.email);
     this.repo$.push(message)
       .then(() => {
-        this.formGroup.reset();
+        this.formGroup.get('comment').reset()
       }).catch(() => {
       this.statusMessage = 'Error while sending feedback';
       this.error = true;
@@ -116,14 +95,5 @@ export class FeedbackWidgetComponent implements OnInit, OnDestroy {
   private getHeaderText(): string {
     const el = this.el.nativeElement.querySelector('h1:not([style*="display:none"]');
     return el ? el.innerHTML : '';
-  }
-
-  private htmlEscape(str) {
-    return str
-      .replace(/&/g, '&amp;')
-      .replace(/"/g, '&quot;')
-      .replace(/'/g, '&#39;')
-      .replace(/</g, '&lt;')
-      .replace(/>/g, '&gt;');
   }
 }
