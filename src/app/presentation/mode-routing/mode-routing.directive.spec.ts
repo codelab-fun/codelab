0f9a783a6c518a141d095a2793426f6ca76ba74d
Preston Van Loon <pvanloon@google.com>
2017-04-30 21:14:43 -0400

    Write test for mode-routing directive and refactor a bit of logic

--- src/app/presentation/mode-routing/mode-routing.directive.spec.ts
+++ src/app/presentation/mode-routing/mode-routing.directive.spec.ts
@@ -1,9 +1,69 @@
+import { Component, HostListener, Output, EventEmitter, DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { ModeRoutingDirective } from './mode-routing.directive';
+import { Mode } from './../mode.enum';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-test-component',
+  template: '<div app-mode-routing></div>'
+})
+class TestComponent {
+  @Output() onModeChange: EventEmitter<Mode> = new EventEmitter();
+}
 
 describe('ModeRoutingDirective', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let directiveEl: DebugElement;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TestComponent,
+        ModeRoutingDirective
+      ],
+      imports: [
+        RouterTestingModule
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(async(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+    directiveEl = fixture.debugElement.query(By.directive(ModeRoutingDirective));
+  }));
+
   it('should create an instance', () => {
-    // TODO: Write this test.
-    // const directive = new ModeRoutingDirective();
-    // expect(directive).toBeTruthy();
+    expect(directiveEl).not.toBeNull();
+    expect(directiveEl).toBeDefined();
   });
+
+  it('should set queryParams onModeChange event of overview', inject([Router], (router: Router) => {
+    const MODE: Mode = 'overview';
+    const componentInstance = fixture.componentInstance;
+
+    const spy = spyOn(router, 'navigate');
+
+    directiveEl.triggerEventHandler('onModeChange', MODE);
+    fixture.detectChanges();
+
+    expect(router.navigate).toHaveBeenCalled();
+    expect(spy.calls.first().args[1].queryParams.mode).toBe(MODE);
+  }));
+
+  it('should not set queryParams onModeChange event of none', inject([Router], (router: Router) => {
+    const MODE: Mode = 'none';
+    const componentInstance = fixture.componentInstance;
+
+    const spy = spyOn(router, 'navigate');
+
+    directiveEl.triggerEventHandler('onModeChange', MODE);
+    fixture.detectChanges();
+
+    expect(router.navigate).toHaveBeenCalled();
+    expect(spy.calls.first().args[1].queryParams).toBeUndefined();
+  }));
 });
