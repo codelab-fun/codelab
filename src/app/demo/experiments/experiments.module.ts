18f6abb5e3e7c30b73d55bfaae288a25a1365df1
Victor Mikhaylov <vic.m@mail.ru>
2017-05-01 14:07:10 -0400

    experiments. updates #5

--- src/app/demo/experiments/experiments.module.ts
+++ src/app/demo/experiments/experiments.module.ts
@@ -5,7 +5,7 @@ import {SlidesRoutes} from '../../presentation/slide-routes';
 import {FeedbackModule} from '../../feedback/feedback.module';
 import {ExperimentsComponent} from './experiments.component';
 import {PresentationModule} from '../../presentation/presentation.module';
-import {ExerciseModule} from "../../exercise/exersice.module";
+import {ExerciseModule} from '../../exercise/exercise.module';
 
 
 const routes = RouterModule.forChild(
