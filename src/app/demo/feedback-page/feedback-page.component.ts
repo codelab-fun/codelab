732e89df678ad4b1313a5178b09e9ca19f554032
ScriptEd User <scripteduser@Codes-MacBook-Air.local>
2017-05-01 23:58:52 -0400

    grouping by name/slide & filtering by done/undone

--- src/app/demo/feedback-page/feedback-page.component.ts
+++ src/app/demo/feedback-page/feedback-page.component.ts
@@ -1,6 +1,51 @@
 import { Component } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
-import {Message} from "../../feedback/message";
+import { Message } from "../../feedback/message";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import { combineLatest } from "rxjs/observable/combineLatest";
+import 'rxjs/add/operator/map'
+import { Observable } from "rxjs/Observable";
+
+type Filter = 'all'|'done'|'notDone';
+type Grouping = 'nothing'|'href'|'name';
+
+ function groupBy(feedback: Array<Message>, grouping: Grouping){
+      const result =  feedback.reduce((result, item)=>{
+        const group = item[grouping];
+        result[group] = result[group] || [];
+        result[group].push(item);        
+        return result;
+      }, {});
+
+      return Object.keys(result).map(key=>({key, value: result[key]}));
+   }
+
+function  group([feedback, grouping]){
+    if(grouping==='nothing'){
+      return [{
+        key: 'Messages',
+        value: feedback
+      }]
+    }
+    if( grouping === 'name' || grouping === 'href'){
+      return groupBy(feedback, grouping);
+    }
+    throw new Error('Unknown grouping: ' + grouping);    
+  }
+
+  function filter([feedback, filter]){
+    if(filter === 'all'){
+      return feedback;
+    }
+
+    if(filter === 'done'){
+      return feedback.filter(message=>message.isDone);
+    }
+
+    if(filter === 'notDone'){
+      return feedback.filter(message=>!message.isDone);
+    }
+  }
 
 @Component({
   selector: 'app-feedback-page',
@@ -8,8 +53,13 @@ import {Message} from "../../feedback/message";
   styleUrls: ['./feedback-page.component.css']
 })
 export class FeedbackPageComponent {
+  messages$: Observable<{ key: string; value: Message; }[]>;
+  filter$ = new BehaviorSubject<Filter>('notDone');
+  group$ = new BehaviorSubject<Grouping>('href');
+
   constructor(private database: AngularFireDatabase) {
   }
+  
   rawFeedbackList: Array<any> = [];
   currentFeedbackList: Message[] = [];
   dates: Array<string> = [''];
@@ -18,20 +68,14 @@ export class FeedbackPageComponent {
   feedbackersNames = [];
 
 
+
+
+  
+
   ngOnInit() {
-    this.database.list('/feedback').subscribe(list => {
-      this.feedbackersCount = {};
-      this.feedbackersNames = [];
-      list.forEach((val) => {
-        this.feedbackersCount[val.name] = this.feedbackersCount[val.name] ? this.feedbackersCount[val.name] + 1 : 1;
-      });
-      this.feedbackersNames = Object.keys(this.feedbackersCount);
-      this.feedbackersNames.sort((a,b) => {
-        return this.feedbackersCount[b] - this.feedbackersCount[a];
-      });
-      this.rawFeedbackList = list;
-      this.currentFeedbackList = this.rawFeedbackList;
-    });
+   const feedback$ = this.database.list('/feedback');
+   const filteredMessages$ = combineLatest(feedback$, this.filter$).map(filter);   
+   this.messages$ = combineLatest(filteredMessages$, this.group$).map(group)  
   }
 
   isDone(eleKey: any, event): void{
