bf4f00e6a7b2eded77ada8346ba3c03260985390
milocosmopolitan <milocosmopolitan@gmail.com>
2017-05-02 22:06:26 -0400

    slide nav shortcuts only works when event target is boby

--- src/app/exercise/exercise/exercise.base.ts
+++ src/app/exercise/exercise/exercise.base.ts
@@ -72,7 +72,8 @@ export class ExerciseBase implements OnDestroy {
   constructor(public slide: SlideComponent, private monacoConfig: MonacoConfigService) {
     this.onActiveUsubscribe = slide.onActive.filter(a => a).subscribe(() => {
       console.log('ACTIVE');
-      slide.disableShortcuts();
+
+      // slide.disableShortcuts();
       slide.disableResize();
       this.loadModels();
     });
