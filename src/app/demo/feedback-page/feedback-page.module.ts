94b95ccbb319e53435478396689124a4c90900d7
Preston Van Loon <pvanloon@google.com>
2017-05-03 19:22:59 -0400

    Move firebase config to environments

--- src/app/demo/feedback-page/feedback-page.module.ts
+++ src/app/demo/feedback-page/feedback-page.module.ts
@@ -8,21 +8,14 @@ import {FormsModule} from '@angular/forms';
 import {BrowserWindowModule} from '../../browser-window/browser-window.module';
 import {PresentationModule} from '../../presentation/presentation.module';
 import { CommonModule } from '@angular/common';
+import { environment } from '../../../environments/environment';
 
 
 const routes = RouterModule.forChild(
   SlidesRoutes.get(FeedbackPageComponent)
 );
 
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBDg_JEXDrn7iuvGR-xrcU1bmjWc-uxmgA',
-  authDomain: 'ng2-codelab.firebaseapp.com',
-  databaseURL: 'https://ng2-codelab.firebaseio.com',
-  storageBucket: 'ng2-codelab.appspot.com'
-};
-
-export const angularFire = AngularFireModule.initializeApp(firebaseConfig);
+export const angularFire = AngularFireModule.initializeApp(environment.firebaseConfig);
 
 @NgModule({
   imports: [routes, BrowserWindowModule, PresentationModule, angularFire, FormsModule, CommonModule],
