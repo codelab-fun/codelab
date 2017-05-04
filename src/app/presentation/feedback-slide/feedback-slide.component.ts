86e86047c82b7fcfe31a8f81d4e8e1bd324c153b
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-04 00:49:04 -0400

    adds functionality to move to next slide onClick (anywhere)

--- src/app/presentation/feedback-slide/feedback-slide.component.ts
+++ src/app/presentation/feedback-slide/feedback-slide.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PresentationComponent } from '../presentation/presentation.component';
 
 @Component({
   selector: 'app-feedback-slide',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedbackSlideComponent implements OnInit {
 
-  constructor() { }
+  constructor(private presentation: PresentationComponent) { }
 
   ngOnInit() {
+  }
+
+  closeSlide() {
+    this.presentation.nextSlide();
   }
 
 }
