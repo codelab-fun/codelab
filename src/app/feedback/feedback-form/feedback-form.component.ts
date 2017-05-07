edbddc044fb374cc57248cd20ba4d9668c82dd6b
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-07 17:01:41 -0400

    edits 'getHeaderText' as per review suggestions

--- src/app/feedback/feedback-form/feedback-form.component.ts
+++ src/app/feedback/feedback-form/feedback-form.component.ts
@@ -50,9 +50,8 @@ export class FeedbackFormComponent implements OnInit {
   }
 
   private getHeaderText(): string {
-    // const el = this.el.element.nativeElement.querySelector('h1:not([style*="display:none"]');
-    // return el ? el.innerHTML : '';
-    return '';
+    const el = document.body.querySelector('h1');
+    return el ? el.innerHTML : '';
   }
 
 }
