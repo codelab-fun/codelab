10efc4e3152b86814a5eca8baf92ea8f9a042d99
Michael Solati <mkslt04@gmail.com>
2017-05-10 19:02:40 -0400

    Fixes #150 Add new descriptions for cats
    Add new cats as well

--- ng2ts/api.service.ts
+++ ng2ts/api.service.ts
@@ -2,71 +2,71 @@ import {VideoItem} from './video/video-item';
 
 const FAKE_VIDEOS = [
   {
-    title: `Cute kitten`,
+    title: 'Scruffy McPurrrr',
     src: '/assets/images/cat-0.png',
-    description: `todo`,
+    description: 'Scruffy McPurrrr is a world famous kitty known for his escapades around the world purring on laps!',
     views: 100,
     likes: 49329,
     date: '2016-11-25'
   },
   {
-    title: `Kitten on the tree`,
+    title: 'The Itty Bitty Kitty Comittee',
     src: '/assets/images/cat-1.jpg',
-    description: 'todo',
+    description: 'The Itty Bitty Kitty Comittee are a collection of felines coming for you!',
     views: 100,
     likes: 20,
     date: '2016-11-21'
   }, {
-    title: 'More kitten',
+    title: 'IT A KITTY!',
     src: '/assets/images/cat-2.jpg',
-    description: 'todo',
+    description: 'It\'s a kitty, I mean, how are you not in love with him?',
     views: 100,
     likes: 20,
     date: '2016-10-02'
 
   }, {
-    title: 'Another kitten',
+    title: 'Cate Purrton\'s C.I. Purr Edition Cover',
     src: '/assets/images/cat-3.jpg',
-    description: 'todo',
+    description: 'We don\'t hear any Cat\'s Illustrated readers complaining about Purrton\'s cover.',
     views: 100,
     likes: 20,
     date: '2016-09-02'
   },
   {
-    title: 'Serious cat',
+    title: 'SINGLE SUNSHINE',
     src: '/assets/images/cat-4.jpg',
-    description: 'todo',
+    description: 'It was cat for at least an hour. It was incredible. The camera could not capture the vivid intensity and brightness. Look into the mirror, look into your soul! What it means.',
     views: 100,
     likes: 20,
     date: '2016-08-02'
   },
   {
-    title: 'Serious cat',
-    src: '/assets/images/cat-5.jpg',
-    description: 'todo',
+    title: 'Mittens',
+    src: '/assets/images/cat-5.png',
+    description: 'Mitty is a kitty with white mittens for paws!',
     views: 100,
     likes: 20,
     date: '2016-08-02'
   }, {
-    title: 'Serious cat',
-    src: '/assets/images/cat-5.jpg',
-    description: 'todo',
+    title: 'Cinco de Gato',
+    src: '/assets/images/cat-6.jpg',
+    description: 'To be totally fair, we also wound up celebrating Moewloween, Purrster, Scratching Day, Good Kitty Day, All Cates Day, and various bank holidays in the exact same way.',
     views: 100,
     likes: 20,
-    date: '2016-08-02'
+    date: '2017-05-05'
   },
   {
     title: 'Oops, looks like a bug!',
     src: '/assets/images/dog.jpg',
-    description: 'todo',
+    description: 'Whoomp there it is...',
     views: 100,
     likes: 20,
     date: '2016-08-02'
   },
   {
-    title: 'Serious cat',
-    src: '/assets/images/cat-6.jpg',
-    description: 'todo',
+    title: 'Koffee Kat',
+    src: '/assets/images/cat-7.jpg',
+    description: 'Somedays you really did have too much coffee... That\'s everyday for Koffee Kat',
     views: 100,
     likes: 20,
     date: '2016-08-02'
