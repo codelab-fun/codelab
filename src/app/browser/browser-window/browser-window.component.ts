4ec9abc05df3a522efe6115eecc8f00aea3bd6bc
joeycatxx <sunilj74@gmail.com>
2017-05-11 16:17:32 -0400

    Added browser module

---
+++ src/app/browser/browser-window/browser-window.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input} from '@angular/core';
+
+@Component({
+  selector: 'slides-browser-window',
+  templateUrl: './browser-window.component.html',
+  styleUrls: ['./browser-window.component.css']
+})
+export class BrowserWindowComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/browser/console-window/console-window.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'slides-console-window',
+  templateUrl: './console-window.component.html',
+  styleUrls: ['./console-window.component.css']
+})
+export class ConsoleWindowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
