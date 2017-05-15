fb3fdce6f33f0aab684c99afeee4027a5118c29c
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-05-14 23:13:54 -0400

    Fix tests

--- src/app/presentation/title-slide/title-slide.component.spec.ts
+++ src/app/presentation/title-slide/title-slide.component.spec.ts
@@ -29,21 +29,21 @@ describe('TitleSlideComponent', () => {
   it('should render a title', () => {
     component.title = 'awesome title';
     fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('div.slide-title'));
+    const el = fixture.debugElement.query(By.css('.header'));
     expect(el.nativeElement.textContent).toContain('awesome title');
   });
 
   it('should render a description', () => {
     component.description = 'hello world!';
     fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('p.slide-title-description'));
+    const el = fixture.debugElement.query(By.css('.description'));
     expect(el.nativeElement.textContent).toContain('hello world!');
   });
 
   it('should render prerequisites', () => {
     component.prereqs = 'do this first';
     fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('p.slide-title-prereqs'));
+    const el = fixture.debugElement.query(By.css('.prereqs'));
     expect(el.nativeElement.textContent).toContain('do this first');
     expect(el.nativeElement.textContent).toContain('Prerequisites');
   });
@@ -51,7 +51,7 @@ describe('TitleSlideComponent', () => {
   it('should not show "Prerequisites" if undefined', () => {
     component.prereqs = undefined;
     fixture.detectChanges();
-    const el = fixture.debugElement.query(By.css('p.slide-title-prereqs'));
+    const el = fixture.debugElement.query(By.css('.prereqs'));
     expect(el.nativeElement.textContent).not.toContain('Prerequisites');
   });
 });
