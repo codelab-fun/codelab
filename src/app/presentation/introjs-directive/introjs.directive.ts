b05f4910e2b12301f8aa124f711904cf359db9eb
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-14 23:58:26 -0400

    comment out tour disabler for now

--- src/app/presentation/introjs-directive/introjs.directive.ts
+++ src/app/presentation/introjs-directive/introjs.directive.ts
@@ -8,17 +8,20 @@ import {introJs} from 'intro.js/intro.js';
 export class IntrojsDirective implements OnInit {
 
   constructor(public slide: SlideComponent) {
-    if (!localStorage.numTours) {
-      localStorage.numTours = 0;
-    }
+    // TODO: add check back in after google i/o
+    // if (!localStorage.numTours) {
+    //   localStorage.numTours = 0;
+    // }
   }
 
   ngOnInit() {
     this.slide.onActive.subscribe((active) => {
       // check if both tours ran TODO: unhardcode this check
-      if (active && localStorage.numTours <= 2) {
+      // if (active && localStorage.numTours <= 2) {
+      if (active) {
         setTimeout(() => introJs().start(), 1000);
-        localStorage.numTours = +localStorage.numTours + 1;
+        // localStorage.numTours = +localStorage.numTours + 1;
+
         // @HostListener('keydown') handleKeyboardEvent(eventData: Event) {
         //   alert("eventData");
         // }
