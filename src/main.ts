3992ee6a6929301c9185829d9d09f9c988a13167
tessaSAC <tessaSAC@users.noreply.github.com>
2017-05-26 23:37:38 -0400

    fix position of tour window for linter warning

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {environment} from './environments/environment';
 import {DemoModule} from './app/demo/demo.module';
 
+declare const require;
+
+require('style-loader!../node_modules/intro.js/minified/introjs.min.css');
+require('style-loader!../node_modules/intro.js/themes/introjs-nassim.css');
+
 if (environment.production) {
   enableProdMode();
 }
