1c06b1db31829b1ddf0f63ba606fe370efe5e35a
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-06-06 20:12:23 -0400

    Get rid of some old code

--- src/app/exercise/services/monaco-config.service.spec.ts
+++ src/app/exercise/services/monaco-config.service.spec.ts
@@ -17,23 +17,4 @@ describe('Service: MonacoConfig', () => {
   it('should exist', inject([MonacoConfigService], (service: MonacoConfigService) => {
     expect(service).toBeTruthy();
   }));
-
-  describe('sorting files', () => {
-    // it('should return things as is if there are no deps.', inject([MonacoConfigService], (service: MonacoConfigService) => {
-    //   const dep1 = {moduleName: '1', template: '', path: '1'};
-    //   const dep2 = {moduleName: '2', template: '', path: '2'};
-    //   const files = [dep1, dep2];
-    //
-    //   const sorted = service.sortFiles(files);
-    //   expect(sorted).toEqual([dep1, dep2]);
-    // }));
-    // it('should do simple sorting', inject([MonacoConfigService], (service: MonacoConfigService) => {
-    //   const dep1 = {moduleName: '1', deps: ['2'], template: '', path: '1'};
-    //   const dep2 = {moduleName: '2', template: '', path: '2'};
-    //   const files = [dep1, dep2];
-    //
-    //   const sorted = service.sortFiles(files);
-    //   expect(sorted).toEqual([dep2, dep1]);
-    // }));
-  });
 });
