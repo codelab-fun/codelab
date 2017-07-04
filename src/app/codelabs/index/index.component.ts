9e9215e7d9ffe703666f46e28b35e94e57ae9c44
Aziz <ayokubjonov@hudsonhs.com>
2017-07-04 19:01:47 -0400

    move out index routes translation

--- src/app/codelabs/index/index.component.ts
+++ src/app/codelabs/index/index.component.ts
@@ -1,21 +1,44 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, ViewChild } from '@angular/core';
+import { extractMessages } from '../../presentation/i18n-tools';
 
 export interface IndexPageRoute {
   name: string;
   description: string;
   page?: string;
 }
+
+const routesTranslations = {
+  'Create your first Angular app': 'createFirstNgApp',
+  'Templates': 'templates',
+  'Dependency-Injection': 'dependencyInjection',
+  'Component-Tree': 'componentTree',
+  'Custom-Events': 'customEvents',
+  'Angular is written in TypeScript, a superset of JavaScript. Learn TypeScript.': 'angularWrittenInTypescript',
+  'Learn how to create and bootstrap your first Angular application': 'learnHowToBootstrapApp',
+  'Learn how to use Angular templates': 'learnUsingTemplates',
+  'Learn how to provide dependencies to your code instead of hard-coding them': 'learnToProvideDependencies',
+  'Learn how to structure your app with reusable components': 'learnToStructureAppWithReusableComponents',
+  'Learn to bind to events.': 'learnToBindToEvents'
+};
+
 @Component({
   selector: 'slides-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
-
   routes: Array<IndexPageRoute>;
+  @ViewChild('translations') translations;
 
   getMainPageRoutes() {
-    return this.getPageRoutes('main');
+    const t = extractMessages(this.translations); // translations from the template
+    return this.getPageRoutes('main').map(route => {
+      return {
+        ...route,
+        name: t[routesTranslations[route.name]] || route.name,
+        description: t[routesTranslations[route.description]] || route.description
+      };
+    });
   }
 
   getBonusPageRoutes() {
