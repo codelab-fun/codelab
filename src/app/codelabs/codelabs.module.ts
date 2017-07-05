a0bacc8143c0646d9aaf96dc9fc29fa0a2061207
Kirill Cherhkashin <kirill@kcherkashin.org>
2017-07-05 16:43:42 +0300

    bunch of stuff

--- src/app/codelabs/codelabs.module.ts
+++ src/app/codelabs/codelabs.module.ts
@@ -137,7 +137,7 @@ export function monacoReady() {
     HttpModule,
     PresentationModule,
     ExerciseModule,
-    RouterModule.forRoot(routes),
+    RouterModule.forRoot(routes, { enableTracing: true }),
     TooltipsModule,
     FeedbackModule
   ],
