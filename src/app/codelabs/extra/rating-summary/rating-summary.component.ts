fc887a7f7a6e5f0808db6f2542777bf4b00f0b35
Sunil Joseph <josephs@mit.mol-liner.com>
2017-07-12 07:59:59 -0400

    added feature for user to rate each lesson

---
+++ src/app/codelabs/extra/rating-summary/rating-summary.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'slides-rating-summary',
+  templateUrl: './rating-summary.component.html',
+  styleUrls: ['./rating-summary.component.css']
+})
+export class RatingSummaryComponent implements OnInit {
+  ngOnInit() {
+  }
+}
