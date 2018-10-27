9f129f980d87ec01acdc2182c826f498f3a8aa08
kirjs <kirill@kcherkashin.org>
2018-10-27 15:32:36 -0400

    Add a smart redirect to get rid of having /0 in the end of URLs

--- libs/presentation/src/lib/slide-routes.ts
+++ libs/presentation/src/lib/slide-routes.ts
@@ -1,8 +1,9 @@
 export class SlidesRoutes {
   static get(Component: any) {
     return [
-      {path: `:id`, component: Component},
-      {path: `**`, component: Component}
+      {path: '', redirectTo: '0', pathMatch: 'full'},
+      {path: ':id', component: Component},
+      {path: '**', component: Component},
     ];
   }
 }
