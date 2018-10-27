42a418448e6c1c0db24ed8f12d8ffb92f1f88420
kirjs <kirill@kcherkashin.org>
2018-10-27 15:16:31 -0400

    Move more stuff

---
+++ apps/kirjs/src/app/modules/gomoku/gomoku.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SlidesRoutes } from '../../../../../../libs/presentation/src/lib/slide-routes';
+
+import { ExerciseModule } from '../../../../../angular-presentation/src/app/exercise/exercise.module';
+import { PresentationModule } from '../../../../../../libs/presentation/src/lib/presentation.module';
+import { FeedbackModule } from '../../../../../../libs/feedback/src/lib/feedback.module';
+import { BrowserWindowModule } from '../../../../../../libs/browser/src/lib/browser.module';
+import { RunnersModule } from '../../../../../angular-presentation/src/app/exercise/runners/runners.module';
+import { GomokuComponent } from './gomoku.component';
+import { TooltipsModule } from '../../../../../../libs/tooltips/src/lib/tooltips.module';
+import { FakeBabelModule } from '../../../../../angular-presentation/src/app/exercise/fake-babel-runner/fake-babel-runner.module';
+import { CommonModule } from '@angular/common';
+import { GomokuBoardModule } from './board/board.module';
+
+
+const routes = RouterModule.forChild(
+  SlidesRoutes.get(GomokuComponent)
+);
+
+@NgModule({
+  imports: [
+    routes,
+    PresentationModule,
+    ExerciseModule,
+    BrowserWindowModule,
+    FeedbackModule,
+    RunnersModule,
+    CommonModule,
+    TooltipsModule,
+    FakeBabelModule,
+    GomokuBoardModule
+  ],
+  declarations: [GomokuComponent],
+  exports: [GomokuComponent]
+})
+export class GomokuModule {
+
+}
