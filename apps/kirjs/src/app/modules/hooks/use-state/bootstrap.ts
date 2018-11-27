b9b30bf6924d836e933e324d7cbfb6086451063a
kirjs <kirill@kcherkashin.org>
2018-11-27 15:19:25 +0300

    kinda works

--- apps/kirjs/src/app/modules/hooks/use-state/bootstrap.ts
+++ apps/kirjs/src/app/modules/hooks/use-state/bootstrap.ts
@@ -2,11 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { ResourceLoader } from '@angular/compiler';
 import { UseStateModule } from './use-state.module';
-
-
 import { default as template } from './use-state.component.html';
 
-debugger;
 // The code below is used to match the Components with the appropriate templates.
 //
 class MyResourceLoader extends ResourceLoader {
