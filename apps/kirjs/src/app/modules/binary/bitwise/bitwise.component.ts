8934c505ef76c176da5b1f6b0aceb87b94d0d102
kirjs <kirill@kcherkashin.org>
2018-12-22 11:40:09 -0500

    things kinda work

---
+++ apps/kirjs/src/app/modules/binary/bitwise/bitwise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'kirjs-bitwise',
+  templateUrl: './bitwise.component.html',
+  styleUrls: ['./bitwise.component.css']
+})
+export class BitwiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
