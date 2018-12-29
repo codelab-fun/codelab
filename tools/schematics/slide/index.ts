b25eaa7ac5fd1f4222f5b9a7266a14b8d4a7377f
kirjs <kirill@kcherkashin.org>
2018-12-28 20:53:29 -0500

    Override HTML

--- tools/schematics/slide/index.ts
+++ tools/schematics/slide/index.ts
@@ -1,7 +1,17 @@
-import { chain, externalSchematic, Rule, Tree, SchematicContext } from '@angular-devkit/schematics';
+import { chain, externalSchematic, Rule, Tree, SchematicContext, url } from '@angular-devkit/schematics';
 import { addImportToModule } from '@schematics/angular/utility/ast-utils';
 import * as ts from 'typescript';
+import * as fs from 'fs';
+import {join} from 'path';
 import { InsertChange } from '@schematics/angular/utility/change';
+
+function overrideHtml(schema: any): Rule {
+  return (host: Tree, context: SchematicContext) => {
+    const path: string = host.actions.find(a => a.path.endsWith('.component.html')).path;
+    const html = fs.readFileSync(join(__dirname, './files/template.component.html'), 'utf-8').toString();    
+    host.overwrite(path, html);
+  }
+}
 
 function importSlidesModule(schema: any): Rule {
   return (host: Tree, context: SchematicContext) => {
@@ -9,8 +19,8 @@ function importSlidesModule(schema: any): Rule {
     // @angular-presentation/slides
     const sourceFile = ts.createSourceFile(
       modulePath,
-      host.read(modulePath).toString('utf-8'), 
-      ts.ScriptTarget.Latest, 
+      host.read(modulePath).toString('utf-8'),
+      ts.ScriptTarget.Latest,
       true
     );
 
@@ -37,6 +47,7 @@ export default function (schema: any): Rule {
       name: schema.name,
       project: schema.project
     }),
-    importSlidesModule(schema)
+    importSlidesModule(schema),
+
   ]);
 }
