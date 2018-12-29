f602bcfc2ba3c6e455b2ad3044c7d7129b834f06
kirjs <kirill@kcherkashin.org>
2018-12-28 20:17:05 -0500

    Take project as a parameter

--- tools/schematics/slide/index.ts
+++ tools/schematics/slide/index.ts
@@ -1,9 +1,11 @@
 import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 
 export default function(schema: any): Rule {
+  console.log("schema", schema);
   return chain([
-    externalSchematic('@nrwl/schematics', 'lib', {
-      name: schema.name
+    externalSchematic('@schematics/angular', 'component', {
+      name: schema.name,
+      project: schema.project
     })
   ]);
 }
