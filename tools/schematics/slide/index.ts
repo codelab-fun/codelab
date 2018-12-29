d5f4479cc70d73ea1a78d84de73c02f4b61a056c
kirjs <kirill@kcherkashin.org>
2018-12-28 20:20:42 -0500

    Generate module and component

--- tools/schematics/slide/index.ts
+++ tools/schematics/slide/index.ts
@@ -3,9 +3,13 @@ import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
 export default function(schema: any): Rule {
   console.log("schema", schema);
   return chain([
+    externalSchematic('@schematics/angular', 'module', {
+      name: schema.name,
+      project: schema.project
+    }),
     externalSchematic('@schematics/angular', 'component', {
       name: schema.name,
       project: schema.project
-    })
+    }),
   ]);
 }
