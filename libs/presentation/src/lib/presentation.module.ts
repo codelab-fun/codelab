c899dd449bbbf251752732fb66c0a21c3ba66078
kirjs <kirill@kcherkashin.org>
2019-01-13 15:49:50 -0500

    Get rid of feedback slide

--- libs/presentation/src/lib/presentation.module.ts
+++ libs/presentation/src/lib/presentation.module.ts
@@ -4,7 +4,6 @@ import { PresentationComponent } from './presentation/presentation.component';
 import { ProgressBarComponent } from './progress-bar/progress-bar.component';
 import { MenuShortcutComponent } from './menu-shortcut/menu-shortcut.component';
 import { ClosingSlideComponent } from './closing-slide/closing-slide.component';
-import { FeedbackSlideComponent } from './feedback-slide/feedback-slide.component';
 import { AnalyticsService } from './analytics.service';
 import { HammerGestureConfig } from '@angular/platform-browser';
 
@@ -23,14 +22,12 @@ import { HammerGestureConfig } from '@angular/platform-browser';
     PresentationComponent,
     ProgressBarComponent,
     MenuShortcutComponent,
-    FeedbackSlideComponent,
     ClosingSlideComponent
   ],
   exports: [
     PresentationComponent,
     ProgressBarComponent,
     MenuShortcutComponent,
-    FeedbackSlideComponent,
     ClosingSlideComponent
   ],
   imports: [
