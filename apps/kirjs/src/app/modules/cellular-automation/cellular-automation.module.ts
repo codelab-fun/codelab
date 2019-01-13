735d82a0cb946cea7b5c274c52d1bd53b3db4782
kirjs <kirill@kcherkashin.org>
2019-01-13 13:22:33 -0500

    Upgrade cellular automata

--- apps/kirjs/src/app/modules/cellular-automation/cellular-automation.module.ts
+++ apps/kirjs/src/app/modules/cellular-automation/cellular-automation.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SlidesRoutes } from '../../../../../../libs/presentation/src/lib/slide-routes';
 import { FeedbackModule } from '../../../../../../libs/feedback/src/lib/feedback.module';
-import { PresentationModule } from '../../../../../../libs/presentation/src/lib/presentation.module';
 import { ExerciseModule } from '../../../../../../libs/exercise/src/lib/exercise.module';
 import { CellularAutomationComponent } from './cellular-automation.component';
 import { BoardComponent } from './board/board.component';
@@ -12,13 +11,14 @@ import { Rule3Component } from './rule3/rule3.component';
 import { RuleComponent } from './rule/rule.component';
 import { Rule8Component } from './rule8/rule8.component';
 import { OscilatorsComponent } from './oscilators/oscilators.component';
+import { SlidesModule } from '@angular-presentation/slides';
 
 const routes = RouterModule.forChild(
   SlidesRoutes.get(CellularAutomationComponent)
 );
 
 @NgModule({
-  imports: [routes, PresentationModule, FeedbackModule, ExerciseModule, CommonModule],
+  imports: [routes, SlidesModule, FeedbackModule, ExerciseModule, CommonModule],
   declarations: [CellularAutomationComponent, BoardComponent, Rule3Component, RuleComponent, Rule8Component, OscilatorsComponent],
   exports: [CellularAutomationComponent]
 })
