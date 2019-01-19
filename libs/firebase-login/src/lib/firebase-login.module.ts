493f71bcbd881ad7dbad29ad5dc4046fc8afb10a
Aziz Yokubjonov <yakubjanov0@hotmail.com>
2019-01-18 21:25:50 -0500

    create login lib

---
+++ libs/firebase-login/src/lib/firebase-login.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginButtonComponent } from './login-button/login-button.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LoginButtonComponent]
+})
+export class FirebaseLoginModule {}
---
+++ libs/firebase-login/src/lib/login-button/login-button.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'angular-presentation-login-button',
+  templateUrl: './login-button.component.html',
+  styleUrls: ['./login-button.component.css']
+})
+export class LoginButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
