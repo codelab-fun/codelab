6a729b7dc6de0c16fb2d4b49b68b67cd151531d0
Aleksandr <abukanov1993@gmail.com>
2019-01-24 09:28:38 +0300

    Add - component hierarchy svg

--- apps/codelab/src/app/codelabs/angular/component-tree/component-tree.module.ts
+++ apps/codelab/src/app/codelabs/angular/component-tree/component-tree.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
-import { ComponentTreeComponent } from './component-tree.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { SlidesRoutes } from '../../../../../../../libs/presentation/src/lib/slide-routes';
+
+import { ComponentTreeComponent } from './component-tree.component';
+import { ComponentsHierarchySvgComponent } from './components-hierarchy-svg';
 
 import { FeedbackModule } from '../../../../../../../libs/feedback/src/lib/feedback.module';
 import { BrowserWindowModule } from '../../../../../../../libs/browser/src/lib/browser.module';
@@ -10,7 +14,6 @@ import { Ng2TsExercises } from '../../../../../../../ng2ts/ng2ts';
 import { CodelabComponentsModule } from '../../../components/codelab-components.module';
 
 import { SlidesModule } from '../../../../../../../libs/slides/src';
-import { FormsModule } from '@angular/forms';
 import { CodeDemoModule } from '../../../../../../../libs/code-demos/src';
 
 const routes = RouterModule.forChild([
@@ -35,7 +38,11 @@ const routes = RouterModule.forChild([
     FormsModule
   ],
   providers: [Ng2TsExercises],
-  declarations: [ComponentTreeComponent],
+  declarations: [
+    ComponentTreeComponent,
+    ComponentsHierarchySvgComponent
+  ],
   exports: [ComponentTreeComponent]
 })
-export class ComponentTreeModule {}
+export class ComponentTreeModule {
+}
