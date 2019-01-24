7b6e9dff451b8336c0a6461faba96a9afefdddc7
ringo kam <ringo_kam@hotmail.com>
2019-01-23 20:54:47 -0500

    indent fix

--- cypress/integration/codelab/home.spec.js
+++ cypress/integration/codelab/home.spec.js
@@ -1,13 +1,13 @@
 context("home page", () => {
 
-            beforeEach(() => {
-                cy.visit('Http://localhost:4200/');
-            })
+    beforeEach(() => {
+        cy.visit('Http://localhost:4200/');
+    })
 
-            it("typescript should be clickable", () => {
-                cy.get('.learn-box.typescript').click()
-                cy.location().should(location => {
-                    expect(location.href).to.eq('http://localhost:4200/typescript/intro')
-                })
-            })
+    it("typescript should be clickable", () => {
+        cy.get('.learn-box.typescript').click()
+        cy.location().should(location => {
+            expect(location.href).to.eq('http://localhost:4200/typescript/intro')
         })
+    })
+})
