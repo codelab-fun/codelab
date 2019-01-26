a412abb48cd6870c9263ec3592ec421d571db997
Kirill Cherkashin <kirill@kcherkashin.org>
2019-01-25 20:41:33 -0500

    Update external-link-directive.directive.ts

--- apps/codelab/src/app/components/external-link-directive/external-link-directive.directive.ts
+++ apps/codelab/src/app/components/external-link-directive/external-link-directive.directive.ts
@@ -3,6 +3,7 @@ import { Directive, Input, ElementRef } from '@angular/core';
 @Directive({
   selector: '[href]'
 })
+// TODO(meinou): Remove the second postfix
 export class ExternalLinkDirectiveDirective {
   constructor(
     {nativeElement}: ElementRef
