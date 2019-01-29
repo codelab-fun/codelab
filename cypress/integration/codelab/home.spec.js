b44cc4683ffa59f7b9b2f7bdc4178ce2a6a24fa2
ringo kam <ringo_kam@hotmail.com>
2019-01-28 19:34:36 -0500

    test pass

--- cypress/integration/codelab/home.spec.js
+++ cypress/integration/codelab/home.spec.js
@@ -1,21 +1,44 @@
-context("home page", () => {
+/// <reference types="Cypress" />
 
-    beforeEach(() => {
-        cy.viewport(2400,  1370);
-        cy.visit('http://localhost:4200/');
-    })
+context('home page', () => {
+  beforeEach(() => {
+    cy.viewport(2400, 1370);
+    cy.visit('http://localhost:4200/');
+  });
 
-    it("didnt click on typescript", () => {
-        cy.matchImageSnapshot("home");
-    })
+  it('home page is the same', () => {
+    cy.matchImageSnapshot('home');
+  });
+
+  it('typescript page should look the same', () => {
+    cy.get('.learn-box.typescript').click().url().then(loc => {
+      let count = 0;
+      check(loc, count);
+    });
+  });
+
+  it("angular page should look the same", () => {
     
-    it("typescript should be clickable", () => {
-        cy.get('.learn-box.typescript').click();
+  })
+});
 
-        // cy.matchImageSnapshot("home");
-
-        cy.location().should(location => {
-            expect(location.href).to.eq('http://localhost:4200/typescript/intro')
-        })
-    })
-})
+function check(prevLocation, count) {
+  count += 1;
+  cy.log(count);
+  //stop checking for screenshot until url is done changing
+  cy.url().should(loc => {
+    expect(typeof(loc) === "string").to.be.true;
+    expect(loc).not.to.eq(prevLocation);
+  });
+  //yay pass, check screenshot and go to next page! 
+  cy.url().then(location => {
+    cy.matchImageSnapshot(location);
+    cy.log(Cypress.$("button").hasClass(".arrow-right.arrow"));
+    if(Cypress.$("button").hasClass("arrow-right")) {
+      cy.get(".arrow-right.arrow").click();
+      check(location, count);
+    } else {
+      cy.log("wohoo! Test is done")
+    }
+  });
+}
