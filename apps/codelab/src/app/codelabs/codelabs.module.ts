c07b8d7754bfb19cddce57263fe02e9b3532c5f6
kirjs <kirill@kcherkashin.org>
2019-02-09 09:41:26 -0500

    Clean up codelabs module

--- apps/codelab/src/app/codelabs/codelabs.module.ts
+++ apps/codelab/src/app/codelabs/codelabs.module.ts
@@ -1,34 +1,24 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 
 import { CodelabsRoutingModule } from './codelabs-routing.module';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from '../shared/shared.module';
-
-import { monacoReady } from '@codelab/code-demos/src/lib/shared/monaco-config.service';
+import { FeedbackModule } from '@codelab/feedback';
+import { SlidesModule } from '@codelab/slides';
 import { FirebaseLoginModule } from '@codelab/firebase-login/src';
 
 @NgModule({
   declarations: [IndexComponent, AppComponent],
   imports: [
-    CommonModule,
-    BrowserModule,
     BrowserAnimationsModule,
-    SharedModule,
+    FeedbackModule,
+    SlidesModule,
     CodelabsRoutingModule,
     FirebaseLoginModule,
   ],
-  providers: [
-    {
-      provide: APP_INITIALIZER,
-      useValue: monacoReady,
-      multi: true
-    }
-  ],
   bootstrap: [AppComponent]
 })
-export class CodelabsModule {}
+export class CodelabsModule {
+}
