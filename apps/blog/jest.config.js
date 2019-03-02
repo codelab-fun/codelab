c710ffd41ca8f4e74d8a6828c9b8c9dc2ced05dc
mrs.leonenko <mrs.leonenko@gmail.com>
2019-03-01 20:19:22 -0500

    init of blog

---
+++ apps/blog/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'blog',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/blog/'
+};
---
+++ apps/blog/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { NxModule } from '@nrwl/nx';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    NxModule.forRoot(),
+    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
