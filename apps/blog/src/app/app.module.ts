5d79f52d79c140234602ad8c7ce93b3c5f85e149
mrs.leonenko <mrs.leonenko@gmail.com>
2019-03-07 19:30:41 -0500

    form create

--- apps/blog/src/app/app.module.ts
+++ apps/blog/src/app/app.module.ts
@@ -5,13 +5,27 @@ import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
 import { RouterModule } from '@angular/router';
 import { FormComponent } from './form/form.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+
 
 @NgModule({
   declarations: [AppComponent, FormComponent],
   imports: [
     BrowserModule,
     NxModule.forRoot(),
-    RouterModule.forRoot([], { initialNavigation: 'enabled' })
+    RouterModule.forRoot([], { initialNavigation: 'enabled' }),
+    MatFormFieldModule,
+    MatSelectModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- apps/blog/src/app/form/form.component.ts
+++ apps/blog/src/app/form/form.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+// import { FormGroup, FormControl } from '@angular/forms';
+// import { REACTIVE_FORM_DIRECTIVES } from '@angular/forms'
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+
 
 @Component({
   selector: 'codelab-form',
@@ -7,9 +20,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
+  myform: FormGroup;
+  title: FormControl;
+  text: FormControl;
+  date: Date;
+  endpoint: String;
+
   constructor() { }
 
   ngOnInit() {
+    this.createFormControls();
+    this.createForm();
+    this.endpoint = '';
+  }
+
+  createFormControls() {
+    this.title = new FormControl('', Validators.required);
+    this.text = new FormControl('', Validators.required);
+
+  }
+
+  createForm() {
+    this.myform = new FormGroup({
+      title: this.title,
+      text: this.text
+    });
+  }
+
+  onSubmit() {
+    if (this.myform.valid) {
+      console.log('Form Submitted!');
+      this.myform.reset();
+    }
   }
 
 }
