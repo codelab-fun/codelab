55698962f699e44adc26b9ee6db3611d2256232f
mrs.leonenko <mrs.leonenko@gmail.com>
2019-04-05 22:15:55 -0400

    vot i vse

--- apps/blog/src/app/form.service.ts
+++ apps/blog/src/app/form.service.ts
@@ -24,26 +24,6 @@ export class FormService {
     private database: AngularFireDatabase,
     private router: Router) { }
 
-    getPreview(text) {
-      const sentences = text.split('.').slice(0, 3);
-      let s = 0;
-      for (let i = 0; i < sentences.length; i++) {
-        s += sentences[i].length;
-      }
-      const limit = 200;
-      const a = [];
-      a.push(s);
-      a.push(text.indexOf('<'));
-      a.push(limit);
-      a.push(text.length);
-      a.sort();
-      console.log(a);
-      let i = 0; while (a[i] < 0) {i++; }
-      const p = a[i];
-      console.log(p);
-      return text.substring(0, p) + '...';
-    }
-
   addPost(
     title: string,
     author: string,
@@ -53,10 +33,8 @@ export class FormService {
       title,
       author,
       text,
-      preview: this.getPreview(text),
       date: new Date().toUTCString()
     };
-    // console.log(this.getPreview(text));
     return this.repo$.push(post);
   }
 }
