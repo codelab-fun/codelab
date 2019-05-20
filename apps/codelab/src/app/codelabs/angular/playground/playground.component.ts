c7475c4372e412ac8019cc2ded3587f5db7e1f57
kirjs <kirill@kcherkashin.org>
2019-05-20 00:12:21 -0400

    Update playgroud to autosubstitute default values

--- apps/codelab/src/app/codelabs/angular/playground/playground.component.ts
+++ apps/codelab/src/app/codelabs/angular/playground/playground.component.ts
@@ -15,7 +15,8 @@ export class PlaygroundComponent {
     const code = activatedRoute.snapshot.queryParams.code;
     if (code) {
       try {
-        this.code = JSON.parse(atob(code));
+
+        this.code = {...angularSampleCode, ...JSON.parse(atob(code))};
       } catch (e) {
         console.log('can not parse code', code);
       }
