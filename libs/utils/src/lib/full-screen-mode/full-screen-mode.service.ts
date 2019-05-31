d87771e6790ecac35f5f8270e4221bdb08396cc9
Lukasz Antkiewicz <dev@lantkiewicz.com>
2019-05-31 12:15:07 -0400

    fix(libs): fixes #924 improves cross-browser fullscreen support

--- libs/utils/src/lib/full-screen-mode/full-screen-mode.service.ts
+++ libs/utils/src/lib/full-screen-mode/full-screen-mode.service.ts
@@ -1,18 +1,75 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+
+interface FullscreenAPIMap {
+  requestFullscreen: string;
+  exitFullscreen: string;
+  fullscreenElement: string;
+}
+
+// Fullscreen API fallback map
+const FN_MAP: { [P in keyof FullscreenAPIMap]: string[] } = {
+  requestFullscreen: [
+    'webkitRequestFullscreen',
+    'webkitRequestFullScreen',
+    'mozRequestFullScreen',
+    'msRequestFullscreen'
+  ],
+  exitFullscreen: [
+    'webkitExitFullscreen',
+    'webkitCancelFullScreen',
+    'mozCancelFullScreen',
+    'msExitFullscreen'
+  ],
+  fullscreenElement: [
+    'webkitFullscreenElement',
+    'webkitCurrentFullScreenElement',
+    'mozFullScreenElement',
+    'msFullscreenElement'
+  ]
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class FullScreenModeService {
 
-  constructor() { }
+  private fn: FullscreenAPIMap;
+
+  constructor(
+    @Inject(DOCUMENT) private document: Document
+  ) {
+    this.fn = Object.keys(FN_MAP).reduce((fns, key) => {
+      fns[key] = [ key, ...FN_MAP[key] ].find(fn =>
+        fn in this.document || fn in this.document.documentElement
+      );
+
+      return fns;
+    }, {}) as FullscreenAPIMap;
+  }
 
   toggleFullScreen() {
-    // check if page is in fullscreen
-    if (document['fullscreenElement']) {
-      document.exitFullscreen();
+    if (this.isFullscreen()) {
+      this.exitFullscreen();
     } else {
-      document.documentElement.requestFullscreen();
+      this.requestFullscreen();
     }
+  }
+
+  isFullscreen() {
+    return !!this.document[this.fn.fullscreenElement];
+  }
+
+  requestFullscreen(el: HTMLElement = null) {
+    const element = el || this.document.documentElement;
+    return this.call(element, this.fn.requestFullscreen);
+  }
+
+  exitFullscreen() {
+    return this.call(this.document, this.fn.exitFullscreen);
+  }
+
+  private call(el: Document | Element, fn: string): Promise<void> {
+    return fn ? el[fn]() : undefined;
   }
 }
