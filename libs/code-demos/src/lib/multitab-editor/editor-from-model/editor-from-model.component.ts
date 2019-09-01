33afda2005cbec661b8d63757730306f51c9ae11
barlanj <barlanj@users.noreply.github.com>
2019-09-01 17:25:31 -0400

    rever changes to monaco files

--- libs/code-demos/src/lib/multitab-editor/editor-from-model/editor-from-model.component.ts
+++ libs/code-demos/src/lib/multitab-editor/editor-from-model/editor-from-model.component.ts
@@ -3,44 +3,31 @@ import {
   Component,
   Input,
   OnDestroy,
-  ViewChild,
-  ChangeDetectionStrategy,
-  HostListener,
-  OnInit,
-  ChangeDetectorRef
+  ViewChild
 } from '@angular/core';
 
 import { MonacoConfigService } from '@codelab/code-demos/src/lib/shared/monaco-config.service';
 import { editor, IDisposable } from 'monaco-editor';
 import ITextModel = editor.ITextModel;
 import { CodeDemoEditorInjector } from '@codelab/code-demos/src/lib/code-demo-editor/code-demo-editor.injector';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/operators';
-import { generateMonacoEditorConfig } from './editor-from-model.config';
 
 @Component({
   selector: 'code-demo-editor-from-model',
   templateUrl: './editor-from-model.component.html',
-  styleUrls: ['./editor-from-model.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./editor-from-model.component.css'],
   providers: [CodeDemoEditorInjector]
 })
-export class EditorFromModelComponent implements OnInit, AfterViewInit, OnDestroy {
-  @ViewChild('editor') el;
-
+export class EditorFromModelComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('editor', { static: false }) el;
   fontSize = 14;
   editor: any;
   height = 0;
   private didChangeListener: IDisposable;
   private model: ITextModel;
-  onContextMenu$: IDisposable;
-
-  stateChanged$ = new Subject();
 
   constructor(
     private editorInjector: CodeDemoEditorInjector,
-    readonly monacoConfigService: MonacoConfigService,
-    private changeDetectorRef: ChangeDetectorRef
+    readonly monacoConfigService: MonacoConfigService
   ) {}
 
   @Input('model') set setModel(model: ITextModel) {
@@ -50,27 +37,40 @@ export class EditorFromModelComponent implements OnInit, AfterViewInit, OnDestro
     }
   }
 
-  private setUpEditor(el: HTMLElement) {
-    return this.monacoConfigService.monaco.editor.create(
-      el, generateMonacoEditorConfig({
-        model: this.model,
-        lineNumbers: true,
-        contextmenu: true
-      })
-    );
+  setUpEditor(el: HTMLElement) {
+    return this.monacoConfigService.monaco.editor.create(el, {
+      wrappingColumn: 10,
+      model: this.model,
+      scrollBeyondLastLine: false,
+      tabCompletion: true,
+      wordBasedSuggestions: true,
+      lineNumbersMinChars: 3,
+      cursorBlinking: 'phase',
+      renderIndentGuides: false,
+      lineNumbers: false,
+      automaticLayout: true,
+      fontSize: this.fontSize,
+      minimap: {
+        enabled: false
+      }
+    });
   }
 
-  private resize() {
+  resize() {
+    const lines = this.editor
+      .getModel()
+      .getValue()
+      .split('\n').length;
+    const lineHeight = this.fontSize * 1.6;
+    const height = Math.max(lines * lineHeight, lineHeight * 5);
 
-  }
-
-  ngOnInit() {
-    // this.stateChanged$.pipe(
-    //   debounceTime(50)
-    // ).subscribe(() => {
-    //   // console.log('stuff');
-    //   // this.resize();
-    // });
+    if (this.height !== height) {
+      this.height = height;
+      this.el.nativeElement.style.height = height + 'px';
+      // Needed for firefox
+      this.el.nativeElement.parentElement.style.height = height + 'px';
+      this.editor.layout();
+    }
   }
 
   ngAfterViewInit() {
@@ -78,43 +78,10 @@ export class EditorFromModelComponent implements OnInit, AfterViewInit, OnDestro
       this.el.nativeElement
     );
 
-    this.onContextMenu$ = this.editor.onContextMenu(e => {
-      this.updateContextMenuElementPosition(e);
-    });
-
-    this.didChangeListener = this.editor.onDidChangeModelContent(() => {
-      this.changeDetectorRef.markForCheck();
-      this.editor.layout();
-    });
-  }
-
-  /**
-   * We need to correct the position of the context menu
-   * when it becomes visible to fixed position due to it
-   * expanding its container height when there is not enough
-   * space. Desired behavior is for it just 'float' on
-   * top of the container context.
-   *
-   * This is a known bug. {@see https://github.com/Microsoft/monaco-editor/issues/1203}
-   *
-   * @todo fix properly when issue is fixed in library.
-   */
-  updateContextMenuElementPosition({ event }) {
-    const menuEl = this.editor.getDomNode().querySelector('.monaco-menu-container') as HTMLElement;
-
-    if (menuEl) {
-      const posY = (event.posy + menuEl.clientHeight) > window.outerHeight
-        ? event.posy - menuEl.clientHeight
-        : event.posy;
-
-      const posX = (event.posx + menuEl.clientWidth) > window.outerWidth
-        ? event.posx - menuEl.clientWidth
-        : event.posx;
-
-      menuEl.style.position = 'fixed';
-      menuEl.style.top =  Math.max(0, Math.floor(posY)) + 'px';
-      menuEl.style.left = Math.max(0, Math.floor(posX)) + 'px';
-    }
+    this.resize();
+    this.didChangeListener = this.editor.onDidChangeModelContent(() =>
+      this.resize()
+    );
   }
 
   ngOnDestroy() {
@@ -123,12 +90,5 @@ export class EditorFromModelComponent implements OnInit, AfterViewInit, OnDestro
       this.didChangeListener.dispose();
       this.didChangeListener = null;
     }
-
-    if (this.onContextMenu$) {
-      this.onContextMenu$.dispose();
-      this.onContextMenu$ = null;
-    }
-
-    this.stateChanged$.unsubscribe();
   }
 }
