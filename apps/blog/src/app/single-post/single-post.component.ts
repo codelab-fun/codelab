a7d14c5f153daad2bef983b9599cbe3f7b12db37
mrs.leonenko <mrs.leonenko@gmail.com>
2019-09-01 14:59:18 -0400

    single-post component

---
+++ apps/blog/src/app/single-post/single-post.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component, OnInit, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Post } from '../form/form.component';
+import { FormService } from '../form.service';
+import { ActivatedRoute } from '@angular/router';
+
+
+
+@Component({
+  selector: 'codelab-single-post',
+  templateUrl: './single-post.component.html',
+  styleUrls: ['./single-post.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class SinglePostComponent implements OnInit {
+//  post$: Observable<Post>;
+//  key: string;
+
+ @Input() post: Post;
+ @Input() full: boolean;
+ @Input() ngClass: any;
+
+ ngOnInit() {}
+
+}
