b72d19397fb9ae16a4df7fbbd4ac94ecb7fcf634
kirjs <kirill@kcherkashin.org>
2019-09-02 18:05:36 -0400

    Fix linter

--- apps/codelab/src/app/codelabs/angular/create-first-app/samples/index-html/bootstrap.ts
+++ apps/codelab/src/app/codelabs/angular/create-first-app/samples/index-html/bootstrap.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'hello-world',
   template: `<h1>Hello I'm an Angular app!</h1>
   <h2>Very soon you will learn how to create and bootstrap me!</h2>
