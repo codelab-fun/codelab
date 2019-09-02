5a4544ddd2424b553823e096cbe2d93c0b8d6699
kirjs <kirill@kcherkashin.org>
2019-09-02 18:26:57 -0400

    clean up

--- apps/angular-thirty-seconds/src/environments/environment.prod.ts
+++ apps/angular-thirty-seconds/src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
   production: true,
-
-  // Firebase
-  // TODO: Create a new firebase app/config for prod deploy.
-  firebaseConfig: {
-    apiKey: 'AIzaSyBDg_JEXDrn7iuvGR-xrcU1bmjWc-uxmgA',
-    authDomain: 'ng2-codelab.firebaseapp.com',
-    databaseURL: 'https://ng2-codelab.firebaseio.com',
-    storageBucket: 'ng2-codelab.appspot.com'
-  }
 };
