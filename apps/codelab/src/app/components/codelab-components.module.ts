4fffa66e28e66855eb36ceec790771c586264f0f
Lukasz Antkiewicz <dev@lantkiewicz.com>
2019-09-05 19:21:43 -0400

    feat(breadcrumb): make breadcrumb milestone into a menu closes #795

--- apps/codelab/src/app/components/codelab-components.module.ts
+++ apps/codelab/src/app/components/codelab-components.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { CodelabExerciseComponent } from './exercise/exercise.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatSelectModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatSelectModule
+} from '@angular/material';
 import { RouterModule } from '@angular/router';
 
 import { SimpleTestsProgressComponent } from './tests-progress/simple-tests-progress.component';
@@ -29,6 +33,7 @@ import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
     FormsModule,
     RouterModule,
     MatButtonModule,
+    MatMenuModule,
     MatSelectModule,
     CodeDemoModule
   ],
