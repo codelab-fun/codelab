a60279499904c3f6bc765d7ef815863187f6f96a
Dima <purisdima@mail.ru>
2019-09-05 23:23:19 +0300

    Fix class names, add onPush strategy

--- apps/codelab/src/app/codelabs/not-found/not-found.component.ts
+++ apps/codelab/src/app/codelabs/not-found/not-found.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'codelab-not-found',
   templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  styleUrls: ['./not-found.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class NotFoundComponent {
   public routes = [
--- apps/codelab/src/app/components/angular-routes/angular-routes.component.ts
+++ apps/codelab/src/app/components/angular-routes/angular-routes.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { menuRoutes } from '../../codelabs/angular/angular-routing.module';
 
 @Component({
   selector: 'codelab-angular-routes',
   templateUrl: 'angular-routes.component.html',
-  styleUrls: ['angular-routes.component.scss']
+  styleUrls: ['angular-routes.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AngularRoutesComponent {
   public menuRoutes = menuRoutes;
