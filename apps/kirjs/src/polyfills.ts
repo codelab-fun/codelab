b3a12aa72de5472047deaf1a01c2330ee7487cc6
kirjs <kirill@kcherkashin.org>
2019-10-10 11:05:35 -0400

    Format

--- apps/kirjs/src/polyfills.ts
+++ apps/kirjs/src/polyfills.ts
@@ -73,4 +73,3 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
