f0ba91812e57c0d7e4163c2a01bd80089efd8a95
kirjs <kirill@kcherkashin.org>
2019-10-10 21:55:32 -0400

    Use different firebase project

--- apps/codelab/src/environments/environment.ts
+++ apps/codelab/src/environments/environment.ts
@@ -8,9 +8,12 @@ export const environment = {
 
   // Firebase
   firebaseConfig: {
-    apiKey: 'AIzaSyBDg_JEXDrn7iuvGR-xrcU1bmjWc-uxmgA',
-    authDomain: 'ng2-codelab.firebaseapp.com',
-    databaseURL: 'https://ng2-codelab.firebaseio.com',
-    storageBucket: 'ng2-codelab.appspot.com'
+    apiKey: "AIzaSyBiY1Lg2RIcKtbgqzfE6Vrg28Zjal6ZWHs",
+    authDomain: "angular-presentation.firebaseapp.com",
+    databaseURL: "https://angular-presentation.firebaseio.com",
+    projectId: "angular-presentation",
+    storageBucket: "angular-presentation.appspot.com",
+    messagingSenderId: "1087862173437",
+    appId: "1:1087862173437:web:0bb7fe324b62580bb31894"
   }
 };
