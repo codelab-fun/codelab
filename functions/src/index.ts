1a58b19abe96498b79189ecff83c701ea3692588
NothingEverHappens <kirjs@google.com>
2019-10-11 09:53:29 -0400

    Update index.ts
    I don't think we should publicly post emails

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,9 +6,7 @@ export const feedbackLogger = functions.database
   .onCreate((snapshot: any) => {
     const feedbackItem = snapshot.val();
     let message = `*${feedbackItem.name}* said "${feedbackItem.comment}" on page https://codelab.fun/${feedbackItem.href}`;
-    message += feedbackItem.email
-      ? `\nYou can get back to *${feedbackItem.name}* at ${feedbackItem.email}`
-      : '';
+
     return fetch(functions.config().slack.url, {
       method: 'POST',
       headers: {
