517766b9a9fe06fa0707e661251431a043cc107f
Matt Erickson <matt.erickson@auth0.com>
2019-10-18 16:29:12 -0500

    And remove the file header with creation date

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration
-// Generated on Fri Oct 18 2019 15:00:24 GMT-0500 (Central Daylight Time)
-
 module.exports = function(config) {
   config.set({
     frameworks: ['jasmine'],
