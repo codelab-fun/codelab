371c440de8fb07f16e11dc1e2074099c79a86543
kirjs <kirill@kcherkashin.org>
2019-10-19 09:18:20 -0400

    chore: Update slide-directive to Ivy

--- libs/slides/src/lib/slide/slide.directive.ts
+++ libs/slides/src/lib/slide/slide.directive.ts
@@ -1,6 +1,9 @@
 import { Directive, Input, TemplateRef } from '@angular/core';
 import { SlidesDeckComponent } from '../deck/deck.component';
 
+const ID_ATTR_NAME = 'id';
+const MILESTONE_ATTR_NAME = 'milestone';
+
 @Directive({
   selector: '[slide]'
 })
@@ -11,16 +14,31 @@ export class SlideDirective {
     private presentation: SlidesDeckComponent,
     private template: TemplateRef<any>
   ) {
-    const attrs = (template as any)._def.element.template.nodes[0].element
-      .attrs;
-    const idAttr = attrs.find(([, name]) => name === 'id');
-    // TODO: Move this to the codelab
-    const milestoneAttr = attrs.find(([, name]) => name === 'milestone');
+    let slide;
 
-    presentation.addSlide({
-      id: idAttr && idAttr[2],
-      milestone: milestoneAttr && milestoneAttr[2],
-      template
-    });
+    // TODO: Move milestone extraction to the codelab.
+    // Ivy
+    if ((template as any)._declarationTContainer) {
+      const attrs = (template as any)._declarationTContainer.attrs || [];
+      const idIndex = attrs.findIndex(n => n === ID_ATTR_NAME);
+      const milestineIndex = attrs.findIndex(n => n === MILESTONE_ATTR_NAME);
+      slide = {
+        id: idIndex !== -1 ? attrs[idIndex + 1] : undefined,
+        milestone: milestineIndex !== -1 ? attrs[milestineIndex + 1] : undefined,
+        template
+      };
+      // Old renderer
+    } else {
+      const attrs = (template as any)._def.element.template.nodes[0].element.attrs;
+      const idAttr = attrs.find(([, name]) => name === ID_ATTR_NAME);
+      const milestoneAttr = attrs.find(([, name]) => name === MILESTONE_ATTR_NAME);
+      slide = {
+        id: idAttr && idAttr[2],
+        milestone: milestoneAttr && milestoneAttr[2],
+        template
+      };
+    }
+
+    presentation.addSlide(slide);
   }
 }
