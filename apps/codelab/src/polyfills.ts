c98b2e51ab972df645d1b2b2d090a802cdf0b944
thekiba <hr@thekiba.io>
2019-10-19 19:13:48 +0300

    added @angular/localize package

--- apps/codelab/src/polyfills.ts
+++ apps/codelab/src/polyfills.ts
@@ -70,3 +70,4 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
 // import 'intl';  // Run `npm install --save intl`.
+import '@angular/localize';
