d856fbb22349fdf87a25c0e923328b85952b66ae
maxim.tsvetkov <maxtsvetapple@gmail.com>
2019-11-03 17:54:36 +0200

    feat: create a slide switching directive

--- apps/codelab/src/app/directives/directives.module.ts
+++ apps/codelab/src/app/directives/directives.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { IsLoggedInDirective } from './permissions/is-logged-in/is-loggen-in.directive';
 import { CanLoadAdminDirective } from './permissions/can-load-admin/can-load-admin.directive';
+import { SwitchSlideDirective } from './switch-slide.directive';
 
 @NgModule({
-  declarations: [IsLoggedInDirective, CanLoadAdminDirective],
-  exports: [IsLoggedInDirective, CanLoadAdminDirective]
+  declarations: [
+    IsLoggedInDirective,
+    CanLoadAdminDirective,
+    SwitchSlideDirective
+  ],
+  exports: [IsLoggedInDirective, CanLoadAdminDirective, SwitchSlideDirective]
 })
 export class DirectivesModule {}
