8fc8376cc2424f7bb657062a519b1c301393b1ab
Marvin Rusinek <marvinrusinek@gmail.com>
2019-11-28 15:09:04 -0500

    Add files via upload
    quiz app

--- apps/playground/src/app/app.component.spec.ts
+++ apps/playground/src/app/app.component.spec.ts
@@ -1,12 +1,16 @@
-import { async, TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, AppModule]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   }));
 
@@ -16,18 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'playground'`, () => {
+  it(`should have as title 'quiz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('playground');
+    expect(app.title).toEqual('quiz');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to playground!'
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('quiz app is running!');
   });
 });
---
+++ apps/playground/src/app/quiz/src/app/containers/introduction/introduction.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-introduction',
+  templateUrl: './introduction.component.html',
+  styleUrls: ['./introduction.component.scss']
+})
+export class IntroductionComponent {
+  QUIZ_TOPIC_IMAGE = '../../../assets/images/dependency-injection-diagram.png';
+
+  constructor(private router: Router) {}
+
+  startQuiz() {
+    this.router.navigateByUrl('/question/1');
+  }
+}
