7a8fbf0d4177e1f476ddfaf12bb980554b74ae6c
kirjs <kirill@kcherkashin.org>
2019-12-05 15:38:56 -0500

    Display list of names

--- libs/utils/src/lib/sync/components/sync-code-game/sync-code-game-admin/sync-code-game-admin.component.ts
+++ libs/utils/src/lib/sync/components/sync-code-game/sync-code-game-admin/sync-code-game-admin.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { SyncRegistrationService } from '@codelab/utils/src/lib/sync/components/registration/sync-registration.service';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -6,8 +7,6 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sync-code-game-admin.component.html',
   styleUrls: ['./sync-code-game-admin.component.css']
 })
-export class SyncCodeGameAdminComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
+export class SyncCodeGameAdminComponent {
+  constructor(readonly registrationService: SyncRegistrationService) {}
 }
