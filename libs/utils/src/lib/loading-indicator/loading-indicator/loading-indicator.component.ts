3e55e533bbecad107f4514daaec94efebc3abf49
kirjs <kirill@kcherkashin.org>
2020-03-03 13:31:30 -0500

    Migrate router component to code-demo

--- libs/utils/src/lib/loading-indicator/loading-indicator/loading-indicator.component.ts
+++ libs/utils/src/lib/loading-indicator/loading-indicator/loading-indicator.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'slides-loading-indicator',
   templateUrl: './loading-indicator.component.html',
   styleUrls: ['./loading-indicator.component.css']
