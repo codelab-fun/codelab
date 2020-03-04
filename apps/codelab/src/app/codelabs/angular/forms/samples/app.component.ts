b50c6a7d5e0abe7cae5949056cb8798edb9067ef
kirjs <kirill@kcherkashin.org>
2020-03-04 14:06:14 -0500

    Update Form milestone to use new interfaces and code-demo-component

---
+++ apps/codelab/src/app/codelabs/angular/forms/samples/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  username = 'Pirojok';
+  email = 'pirojok@angular.io';
+}
