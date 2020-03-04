767466f9fc7e2a36125d7b3461537b300e82a566
kirjs <kirill@kcherkashin.org>
2020-03-04 10:10:02 -0500

    Update Material milestone to use new interfaces

--- apps/codelab/src/app/codelabs/angular/templates/templates.component.ts
+++ apps/codelab/src/app/codelabs/angular/templates/templates.component.ts
@@ -18,12 +18,133 @@ export class TemplatesComponent implements OnInit, MilestoneWithDemos {
     ng2tsConfig.milestones[2].exercises[2],
     ng2tsConfig.milestones[2].exercises[3]
   ];
-  curlies = '{{ property }}';
 
   // TODO(kirjs): we can't access tanslation in OnInit hook iwht static set to false
   // need to consider changing how we set code
   @ViewChild('translations', { static: true }) translation;
-  code: CodeDemos = {};
+  code: CodeDemos = {
+    intro: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/intro/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': [/<h1>.*<\/h1>/]
+      }
+    },
+
+    interpolation: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/interpolation/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': [/{{.*}}/, /firstName = .*/]
+      }
+    },
+
+    expressions: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/expressions/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': [/{{.*}}/, /fullName\(\){/]
+      }
+    },
+
+    dataBindingPre: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/data-binding-pre/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': [/"{{.*}}"/, /avatar = .*/]
+      }
+    },
+
+    dataBinding: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/data-binding/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': /\[.*]/
+      }
+    },
+
+    dataBindingExtra: {
+      code: {
+        'app.component.html': require('!!raw-loader!./samples/data-binding-extra/app.component.html'),
+        'app.component.ts': require('!!raw-loader!./samples/data-binding-extra/app.component.ts'),
+        'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
+        'app.module.ts': require('!!raw-loader!./samples/data-binding-extra/app.module.ts'),
+        'number-praiser.ts': require('!!raw-loader!./samples/data-binding-extra/number-praiser.ts'),
+        'index.html': require('!!raw-loader!./samples/data-binding-extra/index.html')
+      },
+      files: ['app.component.html', 'app.component.ts']
+    },
+
+    ngIf: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/ng-if/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': /\*ngIf/
+      }
+    },
+    repeating: {
+      code: {
+        'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
+        'app.component.ts': require('!!raw-loader!./samples/repeating/app.component.ts')
+      },
+      files: ['app.component.ts'],
+      highlights: {
+        'app.component.ts': ['???', /puppies.*;/]
+      }
+    },
+    eventBinding: {
+      code: {
+        'app.component.html': require('!!raw-loader!./samples/event-binding/app.component.html'),
+        'app.component.ts': require('!!raw-loader!./samples/event-binding/app.component.ts'),
+        'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
+        'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
+        'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
+      },
+      files: ['app.component.html'],
+      highlights: { 'app.component.html': '(click)' }
+    },
+    referenceBinding: {
+      code: {
+        'app.component.html': require('!!raw-loader!./samples/reference-binding/app.component.html'),
+        'app.component.ts': require('!!raw-loader!./samples/reference-binding/app.component.ts'),
+        'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
+        'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
+        'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
+      },
+      files: ['app.component.html'],
+      highlights: { 'app.component.html': ['#input', 'input.value'] }
+    },
+    eventBindingShortcuts: {
+      code: {
+        'app.component.html': require('!!raw-loader!./samples/event-binding-shortcuts/app.component.html'),
+        'app.component.ts': require('!!raw-loader!./samples/reference-binding/app.component.ts'),
+        'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
+        'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
+        'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
+      },
+      files: ['app.component.html'],
+      highlights: { 'app.component.html': '(keydown.control.enter)' }
+    }
+  };
+
   legacyCode = {
     ngForDirective: {
       template: displayAngularComponent(
@@ -62,128 +183,5 @@ describe('AppComponent', ()=>{
 
   ngOnInit() {
     this.t = extractMessages(this.translation);
-
-    this.code = {
-      intro: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/intro/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': [/<h1>.*<\/h1>/]
-        }
-      },
-
-      interpolation: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/interpolation/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': [/{{.*}}/, /firstName = .*/]
-        }
-      },
-
-      expressions: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/expressions/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': [/{{.*}}/, /fullName\(\){/]
-        }
-      },
-
-      dataBindingPre: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/data-binding-pre/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': [/"{{.*}}"/, /avatar = .*/]
-        }
-      },
-
-      dataBinding: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/data-binding/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': /\[.*]/
-        }
-      },
-
-      dataBindingExtra: {
-        code: {
-          'app.component.html': require('!!raw-loader!./samples/data-binding-extra/app.component.html'),
-          'app.component.ts': require('!!raw-loader!./samples/data-binding-extra/app.component.ts'),
-          'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
-          'app.module.ts': require('!!raw-loader!./samples/data-binding-extra/app.module.ts'),
-          'number-praiser.ts': require('!!raw-loader!./samples/data-binding-extra/number-praiser.ts'),
-          'index.html': require('!!raw-loader!./samples/data-binding-extra/index.html')
-        },
-        files: ['app.component.html', 'app.component.ts']
-      },
-
-      ngIf: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/ng-if/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': /\*ngIf/
-        }
-      },
-      repeating: {
-        code: {
-          'bootstrap.ts': require('!!raw-loader!./samples/intro/bootstrap.ts'),
-          'app.component.ts': require('!!raw-loader!./samples/repeating/app.component.ts')
-        },
-        files: ['app.component.ts'],
-        highlights: {
-          'app.component.ts': ['???', /puppies.*;/]
-        }
-      },
-      eventBinding: {
-        code: {
-          'app.component.html': require('!!raw-loader!./samples/event-binding/app.component.html'),
-          'app.component.ts': require('!!raw-loader!./samples/event-binding/app.component.ts'),
-          'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
-          'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
-          'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
-        },
-        files: ['app.component.html'],
-        highlights: { 'app.component.html': '(click)' }
-      },
-      referenceBinding: {
-        code: {
-          'app.component.html': require('!!raw-loader!./samples/reference-binding/app.component.html'),
-          'app.component.ts': require('!!raw-loader!./samples/reference-binding/app.component.ts'),
-          'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
-          'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
-          'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
-        },
-        files: ['app.component.html'],
-        highlights: { 'app.component.html': ['#input', 'input.value'] }
-      },
-      eventBindingShortcuts: {
-        code: {
-          'app.component.html': require('!!raw-loader!./samples/event-binding-shortcuts/app.component.html'),
-          'app.component.ts': require('!!raw-loader!./samples/reference-binding/app.component.ts'),
-          'bootstrap.ts': require('!!raw-loader!./../../../shared/angular-code/bootstrap.ts'),
-          'app.module.ts': require('!!raw-loader!./../../../shared/angular-code/app.module.ts'),
-          'index.html': require('!!raw-loader!./../../../shared/angular-code/index.html')
-        },
-        files: ['app.component.html'],
-        highlights: { 'app.component.html': '(keydown.control.enter)' }
-      }
-    };
   }
 }
