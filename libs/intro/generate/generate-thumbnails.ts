4b621896cefb01ef399eb01ca71c039d50fb58ec
maktarsis <tarsis.maksym@gmail.com>
2020-05-26 07:20:53 +0300

    refactor(intro): remove unnecessary code

--- libs/intro/generate/generate-thumbnails.ts
+++ libs/intro/generate/generate-thumbnails.ts
@@ -2,10 +2,9 @@ import { createWriteStream, existsSync, mkdirSync, outputJSON } from 'fs-extra';
 import { ASSETS_SLIDES_PATH, SLIDES_METADATA_PATH } from './helpers/const';
 import { resolve } from 'path';
 import { get } from 'https';
-import { from } from 'rxjs';
 import { getSlides$ } from './helpers/slides';
 
-getSlides$.subscribe((contentUrls: string[]) => {
+getSlides$.subscribe((contentUrls: string[]): void => {
   if (!existsSync(ASSETS_SLIDES_PATH)) {
     mkdirSync(ASSETS_SLIDES_PATH);
   }
@@ -17,13 +16,11 @@ getSlides$.subscribe((contentUrls: string[]) => {
     });
   });
 
-  from(
-    outputJSON(
-      SLIDES_METADATA_PATH,
-      { count: contentUrls.length, contentUrls },
-      { spaces: 2 }
-    )
-  ).subscribe();
+  outputJSON(
+    SLIDES_METADATA_PATH,
+    { count: contentUrls.length, contentUrls },
+    { spaces: 2 }
+  ).then();
 
   console.log('Completed ✅ ');
 });
