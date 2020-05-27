3f93f228a2834ad4fd08ef710807ae779242acc0
maktarsis <tarsis.maksym@gmail.com>
2020-05-27 21:43:13 +0300

    refactor(slides): update nx slides workspace

--- libs/slides/jest.config.js
+++ libs/slides/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   name: 'slides',
   preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/libs/slides'
+  coverageDirectory: '../../coverage/libs/slides',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
 };
