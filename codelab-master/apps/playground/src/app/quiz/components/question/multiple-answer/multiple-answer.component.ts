4aa8fede63752c4416111e3b0f5cfcb21d4f23f6
Marvin Rusinek <marvinrusinek@gmail.com>
2020-11-26 13:39:45 -0500

    move duplicate css into question.component.scss

--- codelab-master/apps/playground/src/app/quiz/components/question/multiple-answer/multiple-answer.component.ts
+++ codelab-master/apps/playground/src/app/quiz/components/question/multiple-answer/multiple-answer.component.ts
@@ -5,7 +5,9 @@ import { QuizQuestionComponent } from '../question.component';
 @Component({
   selector: "codelab-question-multiple-answer",
   templateUrl: "./multiple-answer.component.html",
-  styleUrls: ["./multiple-answer.component.scss"],
+  styleUrls: ["./multiple-answer.component.scss",
+              "../question.component.scss"
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.ShadowDom
 })
