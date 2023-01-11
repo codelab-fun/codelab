907a3bc9935068b2c80b74c6cebf7c41a8d3e0a1
Alexandr Antonovich <alexandrantonovich325@gmail.com>
2023-01-11 22:38:10 +0200

    fix rebase

--- libs/sandbox-runner/src/lib/script-loader/script-loader.service.ts
+++ libs/sandbox-runner/src/lib/script-loader/script-loader.service.ts
@@ -3,29 +3,30 @@ import { assert } from '@codelab/utils';
 
 declare const require;
 
+const scripts = {
+  mocha: require('!!raw-loader!./assets/js/mocha').default,
+  chai: require('!!raw-loader!chai/chai').default,
+  'test-bootstrap':
+  require('!!raw-loader!./assets/js/test-bootstrap').default,
+  zone:  require('!!raw-loader!./assets/js/zone.js').default,
+  reflectMetadata: require('!!raw-loader!reflect-metadata/Reflect.js').default,
+  'mock-console':
+  require('!!raw-loader!./assets/js/mock-console').default,
+  // TODO(kirjs): not sure we still need vue and react libs
+  // vue: require('!!raw-loader!vue/dist/vue.js').default,
+  // react: require('!!raw-loader!react/umd/react.development.js').default,
+  // 'react-dom': require('!!raw-loader!react-dom/umd/react-dom.development.js').default
+};
+
+type ScriptName = keyof  typeof scripts;
+
 @Injectable({
   providedIn: 'root',
 })
 export class ScriptLoaderService {
-  private readonly scripts = {
-    mocha: require('!!raw-loader!./assets/js/mocha').default,
-    chai: require('!!raw-loader!chai/chai').default,
-    'test-bootstrap':
-      require('!!raw-loader!./assets/js/test-bootstrap').default,
-    shim: require('!!raw-loader!core-js/client/shim.min.js').default,
-    zone: require('!!raw-loader!zone.js/dist/zone.js').default,
-    // 'system-config':
-    //   require('!!raw-loader!../../../assets/runner/js/system-config').default,
-    'mock-console':
-      require('!!raw-loader!./assets/js/mock-console').default,
-    // TODO(kirjs): not sure we still need vue and react libs
-    // vue: require('!!raw-loader!vue/dist/vue.js').default,
-    // react: require('!!raw-loader!react/umd/react.development.js').default,
-    // 'react-dom': require('!!raw-loader!react-dom/umd/react-dom.development.js').default
-  };
 
-  getScript(url) {
-    assert(this.scripts[url]);
-    return this.scripts[url];
+  getScript(url: ScriptName) {
+    assert(scripts[url]);
+    return scripts[url];
   }
 }
