6c336b60861ae18064173063c585289ed689843d
Alexandr Antonovich <alexandrantonovich325@gmail.com>
2023-01-12 22:37:14 +0200

    pre commit git hook

--- libs/browser/src/index.ts
+++ libs/browser/src/index.ts
@@ -1,5 +1,8 @@
 export { BrowserWindowComponent } from './lib/browser-window/browser-window.component';
 export { TerminalWindowComponent } from './lib/terminal-window/terminal-window.component';
-export { PreviewWindowComponent, PreviewWindowType } from './lib/preview-window/preview-window.component';
+export {
+  PreviewWindowComponent,
+  PreviewWindowType,
+} from './lib/preview-window/preview-window.component';
 
 export * from './lib/browser.module';
