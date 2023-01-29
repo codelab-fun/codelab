1fb7729bdc9f727768137bf804b47006f2fc3f45
Alexandr Antonovich <alexandrantonovich325@gmail.com>
2023-01-29 20:58:02 +0200

    remove lint staged

--- lint-staged.config.js
+++
@@ -1,7 +0,0 @@
-const commands = ['npm run affected:lint:write', 'npm run format:write'];
-
-const pattern = `{libs,apps}/!(code-demos/assets)**/*.{ts,json,md,html,?(s)css}`;
-
-module.exports = {
-  [pattern]: commands,
-};
