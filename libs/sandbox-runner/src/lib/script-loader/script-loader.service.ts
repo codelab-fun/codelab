1a37678fa39bc1efb8d77fa2b6e90790444aea10
Alexandr Antonovich <alexandrantonovich325@gmail.com>
2023-01-31 21:45:08 +0200

    lint

--- libs/sandbox-runner/src/lib/script-loader/script-loader.service.ts
+++ libs/sandbox-runner/src/lib/script-loader/script-loader.service.ts
@@ -6,25 +6,22 @@ declare const require;
 const scripts = {
   mocha: require('!!raw-loader!./assets/js/mocha').default,
   chai: require('!!raw-loader!chai/chai').default,
-  'test-bootstrap':
-  require('!!raw-loader!./assets/js/test-bootstrap').default,
-  zone:  require('!!raw-loader!./assets/js/zone.js').default,
+  'test-bootstrap': require('!!raw-loader!./assets/js/test-bootstrap').default,
+  zone: require('!!raw-loader!./assets/js/zone.js').default,
   reflectMetadata: require('!!raw-loader!reflect-metadata/Reflect.js').default,
-  'mock-console':
-  require('!!raw-loader!./assets/js/mock-console').default,
+  'mock-console': require('!!raw-loader!./assets/js/mock-console').default,
   // TODO(kirjs): not sure we still need vue and react libs
   // vue: require('!!raw-loader!vue/dist/vue.js').default,
   // react: require('!!raw-loader!react/umd/react.development.js').default,
   // 'react-dom': require('!!raw-loader!react-dom/umd/react-dom.development.js').default
 };
 
-type ScriptName = keyof  typeof scripts;
+type ScriptName = keyof typeof scripts;
 
 @Injectable({
   providedIn: 'root',
 })
 export class ScriptLoaderService {
-
   getScript(url: ScriptName) {
     assert(scripts[url]);
     return scripts[url];
